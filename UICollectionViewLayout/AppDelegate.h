acb7f579f9940c6c6b64a85f127e1e132c221d81
zhangyafeng123 <1240334911@qq.com>
2018-07-03 10:59:54 +0800

    Initial Commit

---
+++ UICollectionViewLayout/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UICollectionViewLayout
+//
+//  Created by linjianguo on 2018/7/3.
+//  Copyright © 2018年 com.justsee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ UICollectionViewLayout/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  UICollectionViewLayout
+//
+//  Created by linjianguo on 2018/7/3.
+//  Copyright © 2018年 com.justsee. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
